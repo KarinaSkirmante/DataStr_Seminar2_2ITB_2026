e3e8bc57c06f5f9c94b69cd1f5311b28e5708008
KarinaSkirmante <karina.krinkele@gmail.com>
2026-03-31 11:51:53 +0300

    Add: added mainService with MyLinkedList testing for add and print

---
+++ src/service/MainService.java
@@ -0,0 +1,24 @@
+package service;
+
+import datastr.MyLinkedList;
+
+public class MainService {
+
+	public static void main(String[] args) {
+		MyLinkedList<Character> symbols = new MyLinkedList<Character>();
+		
+		try
+		{
+			symbols.add('A');//A
+			symbols.add('B');//B
+			symbols.add('C');//C
+			symbols.print();//A B C
+			symbols.add('Z');//A B C Z
+			symbols.print();//A B C Z
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
